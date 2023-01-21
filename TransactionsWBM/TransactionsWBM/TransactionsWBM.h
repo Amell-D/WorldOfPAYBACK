ad638799a5559a44740c0b6d8f366b78e629838a
Amel Dizdarevic <amel.dizdarevic@getdreams.com>
2023-01-21 12:52:21 +0100

    WB-3: Added all neccessary modules for the project

---
+++ TransactionsWBM/TransactionsWBM/TransactionsWBM.h
@@ -0,0 +1,18 @@
+//
+//  TransactionsWBM.h
+//  TransactionsWBM
+//
+//  Created by Amel Dizdarevic on 1/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TransactionsWBM.
+FOUNDATION_EXPORT double TransactionsWBMVersionNumber;
+
+//! Project version string for TransactionsWBM.
+FOUNDATION_EXPORT const unsigned char TransactionsWBMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransactionsWBM/PublicHeader.h>
+
+
